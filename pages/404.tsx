7b02fade2008d1725e0c47c4b3f1eb8570bc24e8
igor.raphael <igor.raphael@openk.com.br>
2022-05-30 18:01:59 -0300

    fix(not-found page): fix error 500 instead 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,55 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-import { rem } from "styles/style";
-import { textColor2 } from "styles/style";
-
-const NotFoundContainer = styled.main`
-  display: block;
-  height: 110vh;
-`;
-
-const NotFoundTitle = styled.p`
-  font-size: ${rem(100)};
-  font-weight: bold;
-  margin-bottom: 0;
-`;
-
-const Small = styled.small`
-  display: block;
-  padding-bottom: 300px;
-  color: rgba(255, 255, 255, 0.5);
-`;
-
+import { useRouter } from "next/router";
 function NotFound() {
-  const [catPic, setCatPic] = React.useState<string>("");
+  const router = useRouter();
 
-  async function getCatsPic() {
-    const pics = await (await fetch("https://cataas.com/cat?json=true")).json();
-    pics.url && setCatPic(`https://cataas.com/${pics.url}`);
-  }
-
+  //Next V12.1.5 dont support 301 redirect in SSR/SSG inside 404 page
   React.useEffect(() => {
-    getCatsPic();
-  }, []);
-
-  return (
-    <NotFoundContainer>
-      <NotFoundTitle>404.</NotFoundTitle>
-      <h1>Parece que essa não era a página que você estava buscando...</h1>
-      {catPic && (
-        <>
-          <h4>Ei ei, não fique triste, olha essa coisa fofa!</h4>
-          <img src={catPic} alt="Gatinho" />
-          <Small>
-            Foto de: <b>Cat as a Service</b>,{" "}
-            <u>
-              <a href="https://cataas.com">cataas.com</a>
-            </u>
-          </Small>
-        </>
-      )}
-    </NotFoundContainer>
-  );
+    router.push("/not-found");
+  });
+  return null;
 }
 
 export default NotFound;
