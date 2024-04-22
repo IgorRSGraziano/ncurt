9856a69803069319084518e64e1626741749ac56
Igor Graziano <igorrsgraziano@gmail.com>
2024-04-22 20:20:38 -0300

    added search input validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { WandSparkles } from "lucide-react";
+import { Search, WandSparkles } from "lucide-react";
 
 export default function Home() {
 	return (
 		<div>
 			<h1 className="text-3xl text-center font-bold">Encurte URL's de forma simples.</h1>
-			<div className="mt-8">
+			<div className="mt-12">
 				<p className="text-center mb-4 text-xl">Digite a URL que deseja encurtar</p>
 				<div className="flex gap-2 items-center justify-center">
 					<Input type="text" placeholder="https://site.com" className="w-full rounded-lg bg-background pr-8 md:w-[200px] lg:w-[500px]" />
@@ -16,6 +16,17 @@ export default function Home() {
 					</Button>
 				</div>
 				<p className="text-center mb-4 text-muted-foreground text-sm mt-2">Você pode enviar uma lista de URL's passando um ";" entre elas</p>
+			</div>
+
+			<div className="my-12">
+				<p className="text-center mb-4 text-xl">Deseja saber aonde uma URL leva?</p>
+				<div className="flex gap-2 items-center justify-center">
+					<Input type="text" placeholder="https://site.com" className="w-full rounded-lg bg-background pr-8 md:w-[200px] lg:w-[500px]" />
+					<Button variant={"default"} className="flex items-center gap-2">
+						<p>Gerar</p>
+						<Search className="h-4 w-4" />
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
