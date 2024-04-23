66db49ab66f03e6c622363ea5a30a9ac96751373
Igor Graziano <igorrsgraziano@gmail.com>
2024-04-22 21:16:56 -0300

    changed target type

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ function Footer({}: Props) {
 		<footer className="fixed bottom-0 w-full border-t border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2 mt-4">
 			<div className="mr-4 flex justify-center items-center gap-8">
 				<p>Feito com ❤️ por Igor</p>
-				<Link href={"https://github.com/IgorRSGraziano"} className="transition-colors hover:text-foreground/80 text-foreground/60">
+				<Link href={"https://github.com/IgorRSGraziano"} target="_blank" className="transition-colors hover:text-foreground/80 text-foreground/60">
 					<Github />
 				</Link>
 			</div>
